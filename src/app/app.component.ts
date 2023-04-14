83af3c15d64d5fc3714eafe5af926efc6c89ca3d
kobatime <kobaledajona@gmail.com>
2023-04-13 19:17:45 -0500

    Add: Calculadora funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   titulo = 'Aplicación de Calculadora';
+  A: number = 0;
+  B: number = 0;
+  suma: number = 0;
+
+  sumarDatos():number{
+    return this.suma = this.A+this.B;
+  }
 }
